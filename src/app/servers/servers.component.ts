3d2f18b4b2ba9a6f2dcec71889a6c97e06a2525a
Bonthala <sai.pranav.bonthala@accenture.com>
2021-03-05 21:15:42 +0530

    Querying route parameters

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ServersService } from './servers.service';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  public servers: {id: number, name: string, status: string}[] = [];
+
+  constructor(private serversService: ServersService, private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.servers = this.serversService.getServers();
+  }
+
+  onReload() {
+    // this.router.navigate(['/servers']);
+    // this.router.navigate(['servers']); // Works same as the above code as navigate Function doesn't know the exact loaction of current active route and it will always appends to the default home route
+    // this.router.navigate(['servers'], {relativeTo: this.route});
+  }
+
+}
